e5dbe6bf77a98b66f1d509e42ceea105c89e143f
sukanyaparashar <sukanyaparashar111@gmail.com>
2024-06-10 16:09:50 +0530

    Add code to get the SPL address from the event logs

--- ERC20ForSPL/scripts/deployERC20ForSPLMintableThruFactory.js
+++ ERC20ForSPL/scripts/deployERC20ForSPLMintableThruFactory.js
@@ -4,31 +4,33 @@
 // You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
 // will compile your contracts, add the Hardhat Runtime Environment's members to the
 // global scope, and execute the script.
-const { ethers, upgrades } = require('hardhat');
-const { expect } = require('chai');
+const { ethers } = require("hardhat");
 
 async function main() {
-    [owner] = ethers.getSigners();
+  const ERC20ForSPLFactoryAddress = "";
+  const ERC20ForSPLFactoryInstance = await ethers.getContractAt(
+    "ERC20ForSPLFactory",
+    ERC20ForSPLFactoryAddress
+  );
 
-    const ERC20ForSPLFactoryAddress = '';
-    const ERC20ForSPLFactoryInstance = await ethers.getContractAt('ERC20ForSPLFactory', ERC20ForSPLFactoryAddress);
-
-    let tx = await ERC20ForSPLFactoryInstance.deploy(
-        'Test Token',
-        'TST',
-        'http://integration-test-link.neoninfra.xyz/erc20forspl-info.json',
-        9,
-        owner.address
-    );
-    await tx.wait(3);
-
-    let mintableToken = await ERC20ForSPLFactoryInstance.tokens();
-    console.log(mintableToken[mintableToken.length - 1], 'tokensData(TOKEN_MINT)');
+  let tx = await ERC20ForSPLFactoryInstance.deploy(
+    "Test Token",
+    "TST",
+    "http://integration-test-link.neoninfra.xyz/erc20forspl-info.json",
+    9
+  );
+  const receipt = await tx.wait(3);
+  console.log("Token Mint:", receipt.logs[3].args[0]);
+  console.log(
+    "Token SPL Address:",
+    bs58.encode(Buffer.from(receipt.logs[3].args[0].slice(2), "hex"))
+  );
+  console.log("Token ERC20 Interface Address:", receipt.logs[3].args[1]);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
 main().catch((error) => {
-    console.error(error);
-    process.exitCode = 1;
+  console.error(error);
+  process.exitCode = 1;
 });
